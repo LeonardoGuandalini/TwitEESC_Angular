d889c60e750a3f3b0a7eea430ff8aec0e36a48e4
Leonardo Guandalini <leoguanda@gmail.com>
2020-04-26 19:20:05 -0300

    Created the three basic components and added routing

--- TwitEESC/src/app/app-routing.module.ts
+++ TwitEESC/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { FeedComponent } from './feed/feed.component';
+import { RegisterComponent } from './register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "login", component: LoginComponent},
+  {path: "feed", component: FeedComponent},
+  {path: "register", component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
